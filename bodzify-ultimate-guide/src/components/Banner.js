745a4ce16ef96914b8caf9fc4ea2439cc5e1fff1
mignot <andreas.garcia@bodzify.com>
2024-01-18 15:28:58 +0100

    Introduce styles

--- bodzify-ultimate-guide/src/components/Banner.js
+++ bodzify-ultimate-guide/src/components/Banner.js
@@ -1,9 +1,13 @@
+import '../styles/Banner.css'
+import logo from '../assets/ouai.jpg'
 import React from 'react'
 import Header from './Header'
 import Description from './Description'
 
 function Banner() {
-    return (<div>
+    return (
+    <div className='banner'>
+        <img src={logo} className='banner-logo' alt='logo' />
         <Header />
         <Description />
     </div>)
