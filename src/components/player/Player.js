62393c44c6ba571248ad19703c3044e7b2bcf06f
mignot <andreas.garcia@bodzify.com>
2024-03-12 13:18:58 +0100

    Fix format issue when loading track in howler

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -65,7 +65,7 @@ const Player = ({isLoggedIn}) => {
             html5={true}
             playing={playing}
             onLoad={() => console.log('loaded')}
-            format={[libraryTrack.extension]}
+            format={[libraryTrack.file_extension.replace('.', '')]}
             onLoadError={handleLoadError}
           />
           <Button onClick={handlePlay}>Play</Button>
