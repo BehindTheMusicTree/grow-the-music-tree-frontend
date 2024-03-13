99f71e1ba3a59a1b84ac105ae6367d3af52c1b19
mignot <andreas.garcia@bodzify.com>
2024-03-13 20:16:17 +0100

    Enable token refreshing

--- src/service/apiService.js
+++ src/service/apiService.js
@@ -2,56 +2,105 @@ import config from '../config/config';
 import axios from 'axios';
 
 const ApiService = {
+  credentials: { username: config.username, password: config.password },
+
   getToken: () => {
     return JSON.parse(localStorage.getItem('jwtToken'));
   },
 
   setToken: (jwtToken) => {
     localStorage.setItem('jwtToken', JSON.stringify(jwtToken));
+    console.log('Token set:', jwtToken);
+  },
+  
+  refreshToken: async () => {
+    const refreshToken = ApiService.getToken().refresh;
+    console.log('Refresh token:', refreshToken);
+    if (refreshToken) {
+      try {
+        console.log('Refreshing token');
+        const response = await axios.post(`${config.apiBaseUrl}auth/token/refresh/`, {
+          refresh: refreshToken,
+        });
+        if (response.status === 200) {
+          console.log('Token refreshed:', response.data);
+          let newToken = ApiService.getToken();
+          newToken.access = response.data.access;
+          ApiService.setToken(newToken);
+        }
+      } catch (error) {
+        // Gérer l'erreur
+      }
+    }
+    else {
+      console.log('No refresh token, logging in');
+      await ApiService.login();
+    }
   },
 
-  getHeaders: () => {
-    const accessToken = ApiService.getToken().access;
+  getHeaders: async () => {
+    console.log('Getting headers');
+    let accessToken = ApiService.getToken().access;
+    console.log('Access token:', accessToken);
     if (accessToken) {
+      try {
+        const payload = JSON.parse(atob(accessToken.split('.')[1]));
+        const expDate = new Date(payload.exp * 1000);
+        console.log('Token expiration:', expDate);
+        if (expDate < new Date()) {
+          console.log('Token expired, refreshing');
+          await ApiService.refreshToken();
+          accessToken = ApiService.getToken().access;
+        }
+        else {
+          console.log('Token still valid');
+        }
+      } catch (error) {
+        // handle error
+      }
       return {
         'Authorization': `Bearer ${accessToken}`,
         'Content-Type': 'application/json',
       };
     }
-    return {
-      'Content-Type': 'application/json',
-    };
+    else {
+      await ApiService.login();
+      return ApiService.getHeaders();
+    }
   },
 
-  login: async (credentials) => {
+  login: async () => {
     fetch(`${config.apiBaseUrl}auth/token/`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(credentials),
+      body: JSON.stringify(ApiService.credentials),
     })
     .then((response) => {
       if (!response.ok) {
         throw new Error(`Login failed with status: ${response.status}`);
       }
-      
+
       response.json()
       .then((responseJson) => {
+        console.log('Login successful:', responseJson);
         ApiService.setToken(responseJson);
       })
     })
   },
 
   fetchData: async (endpoint, method, data = null, page = null) => {
+    console.log('Fetching data:', endpoint);
     let url = `${config.apiBaseUrl}${endpoint}`
     if (page) {
       url += `?page=${page}`
     }
 
+    const headers = await ApiService.getHeaders();
     const response = await fetch(url, {
       method,
-      headers: ApiService.getHeaders(),
+      headers: headers,
       body: data ? JSON.stringify(data) : null,
     })
       .then(async (response) => {
