511a08e679cc7456c03007ddfcd4aa6a664ec59e
mignot <andreas.garcia@bodzify.com>
2024-03-17 09:34:03 +0100

    Display track progressbar

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -1,5 +1,5 @@
 import styles from './Player.module.scss'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import PropTypes from 'prop-types';
 import ReactHowler from 'react-howler';
 import Button from '../button/Button';
@@ -11,14 +11,10 @@ const Player = ({playingLibraryTrack, setPlayingLibraryTrack}) => {
   const [mustLoadStream, setIsLoadingStream] = useState(false);
   const [blobUrl, setBlobUrl] = useState();
   const [playing, setPlaying] = useState(false);
+  const [seek, setSeek] = useState(0);
 
-  const handlePlay = () => {
-    setPlaying(true);
-  }
-
-  const handlePause = () => {
-    setPlaying(false);
-  }
+  const seekInterval = useRef(null);
+  const playerRef = useRef(null);
 
   const handleLoadError = (id, err) => {
     console.log(`Error loading track of blob url ${blobUrl}: ${err}`);
@@ -34,6 +30,45 @@ const Player = ({playingLibraryTrack, setPlayingLibraryTrack}) => {
     const blobUrl = await ApiService.getLibraryTrackAudio(playingLibraryTrack.relativeUrl)
     setBlobUrl(blobUrl);
   }
+  
+  const updateSeek = () => {
+    if (playerRef.current) {
+      const newSeek = playerRef.current.seek();
+      setSeek(newSeek);
+    }
+  }
+  
+  const handlePause = () => {
+    setPlaying(false);
+    clearInterval(seekInterval);
+  }
+  
+  const handlePlay = () => {
+    setPlaying(true);
+    seekInterval.current = setInterval(() => {
+      updateSeek();
+    }, 1000);
+  }
+  
+  const handleSeekingChange = (newSeek) => {
+    if (playerRef.current) {
+      playerRef.current.seek(newSeek);
+    }
+  }
+  
+  useEffect(() => {
+    // Créez l'intervalle lorsque le composant est monté
+    seekInterval.current = setInterval(() => {
+      if (playing) {
+        updateSeek();
+      }
+    }, 1000);
+  
+    // Nettoyez l'intervalle lorsque le composant est démonté
+    return () => {
+      clearInterval(seekInterval);
+    };
+  }, []);
 
   useEffect(() => {
     if (!mustLoadStream) {
@@ -56,21 +91,41 @@ const Player = ({playingLibraryTrack, setPlayingLibraryTrack}) => {
 
   return (
     <div className={styles.Player}>
-      {blobUrl ?  (
-        <>
-          <ReactHowler
-            src={[blobUrl]}
-            html5={true}
-            playing={playing}
-            format={[playingLibraryTrack.fileExtension.replace('.', '')]}
-            onLoadError={handleLoadError}
-          />
-          <Button onClick={handlePlay}>Play</Button>
-          <Button onClick={handlePause}>Pause</Button>
-        </>
-      ) : (
-        <p>Loading audio stream...</p>
-      )}
+      <div className={styles.PlayerButtons}>
+        {blobUrl ?  (
+          <>
+            <ReactHowler
+              ref={playerRef}
+              src={[blobUrl]}
+              html5={true}
+              playing={playing}
+              format={[playingLibraryTrack.fileExtension.replace('.', '')]}
+              onLoadError={handleLoadError}
+            />
+            <Button onClick={handlePlay}>Play</Button>
+            <Button onClick={handlePause}>Pause</Button>
+          </>
+        ) : (
+          <p>Loading audio stream...</p>
+        )}
+      </div>
+      <div className={styles.PlayerSeek}>
+          <label>
+            Seek:
+            <span className='slider-container'>
+              <input
+                type='range'
+                min='0'
+                max={playingLibraryTrack ? playingLibraryTrack.duration.toFixed(2) : 0}
+                step='.01'
+                value={seek}
+                onChange={handleSeekingChange}
+                // onMouseDown={handleMouseDownSeek}
+                // onMouseUp={handleMouseUpSeek}
+              />
+            </span>
+          </label>
+        </div>
     </div>
   )
 }
