0ffb23205c645261b2f7431f0fcf5f333245ca4d
mignot <andreas.garcia@bodzify.com>
2024-04-03 18:43:11 +0200

    Display playlist type

--- src/components/content-area/track-list-sidebar/TrackListSidebar.js
+++ src/components/content-area/track-list-sidebar/TrackListSidebar.js
@@ -3,12 +3,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TrackElement from './track-element/TrackElement';
 
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
 export default function TrackListSidebar({playlistPlayObject}) {
     return (
         <div className={styles.TrackListSidebar}>
             <div className={styles.Header}>
                 <div className={styles.Name}>{playlistPlayObject.contentObject.name}</div>
-                <div className={styles.Info}>{playlistPlayObject.contentObject.libraryTracksCount + " tracks"}</div>
+                <div className={styles.Info}>
+                    {"• " + capitalizeFirstLetter(playlistPlayObject.contentObject.type) + " playlist • "}
+                    {playlistPlayObject.contentObject.libraryTracksCount + " tracks •"}
+                </div>
             </div>
             <div>
                 <ul>
