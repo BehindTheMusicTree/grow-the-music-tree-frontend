27636a7aa0617728bcd3ddf396784a8b9ea5ac43
mignot <andreas.garcia@bodzify.com>
2024-04-05 18:00:35 +0200

    Enable track file selection

--- src/components/content-area/tree-graph/TreeGraph.js
+++ src/components/content-area/tree-graph/TreeGraph.js
@@ -4,7 +4,7 @@ import * as d3 from 'd3';
 import PropTypes from 'prop-types';
 import { PLAY_STATES, GENRE_TREE_RECT_DIMENSIONS } from '../../../constants';
 import ReactDOMServer from 'react-dom/server';
-import { FaSpinner } from 'react-icons/fa';
+import { FaSpinner, FaFileUpload } from 'react-icons/fa';
 
 export default function TreeGraph (
   { genres, handleGenreAddClick, selectPlaylistUuidToPlay, playState, playingPlaylistUuidWithLoadingState }) {
@@ -17,9 +17,14 @@ export default function TreeGraph (
     GENRE_TREE_RECT_DIMENSIONS.HEIGHT + VERTICAL_SEPARATOON_BETWEEN_RECTANGLES
   )
 
-  const FA_ICON_SIZE = 14;
-
   const svgRef = useRef(null);
+  const fileInputRef = useRef(null);
+  const selectingFileGenreUuidRef = useRef(null);
+
+  function handleFileChange(event) {
+    const file = event.target.files[0];
+    alert('File ' + file.name + ' has been selected');
+  }
 
   const buildTreeHierarchy = () => {
     return d3.stratify()
@@ -105,27 +110,33 @@ export default function TreeGraph (
       .text(function(d) {
         return d.data.name;
       });
+  
+    const TRACK_UPLOADED_BUTTON_OFFSET = 10;
+    const PLUS_BUTTON_OFFSET = TRACK_UPLOADED_BUTTON_OFFSET + 13;
+    const PLAYLIST_TRACKS_COUNT_TEXT_OFFSET = PLUS_BUTTON_OFFSET + 13;
+    const PLAY_PAUSE_BUTTON_OFFSET = PLAYLIST_TRACKS_COUNT_TEXT_OFFSET + 13;
 
+    const SPINNER_ICON_SIZE = 14;
     nodes.append('foreignObject')
       .attr('class', styles.SpinnerContainer)
-      .attr('width', FA_ICON_SIZE)
-      .attr('height', FA_ICON_SIZE)
+      .attr('width', SPINNER_ICON_SIZE)
+      .attr('height', SPINNER_ICON_SIZE)
       .attr('dominant-baseline', 'middle')
-      .attr('x', GENRE_TREE_RECT_DIMENSIONS.WIDTH / 2 - 33 - FA_ICON_SIZE / 2)
-      .attr('y', - GENRE_TREE_RECT_DIMENSIONS.HEIGHT / 2 + FA_ICON_SIZE / 2)
+      .attr('x', GENRE_TREE_RECT_DIMENSIONS.WIDTH / 2 - PLAY_PAUSE_BUTTON_OFFSET - SPINNER_ICON_SIZE / 2)
+      .attr('y', - GENRE_TREE_RECT_DIMENSIONS.HEIGHT / 2 + SPINNER_ICON_SIZE / 2)
       .html(function(d) {
         if (playingPlaylistUuidWithLoadingState 
           && playingPlaylistUuidWithLoadingState.uuid === d.data.criteriaPlaylist.uuid
           && playingPlaylistUuidWithLoadingState.isLoading) {
-            return ReactDOMServer.renderToString(<FaSpinner size={FA_ICON_SIZE} className={styles.Spinner}/>)
+            return ReactDOMServer.renderToString(<FaSpinner size={SPINNER_ICON_SIZE} className={styles.Spinner}/>)
           }
       })
-  
+
     nodes.append('text')
       .attr('class', styles.PlayPauseButton)
       .attr('dominant-baseline', 'middle')
       .attr('text-anchor', 'middle')
-      .attr('x', GENRE_TREE_RECT_DIMENSIONS.WIDTH / 2 - 33)
+      .attr('x', GENRE_TREE_RECT_DIMENSIONS.WIDTH / 2 - PLAY_PAUSE_BUTTON_OFFSET)
       .attr('y', 0)
       .style('visibility', function(d) {
         playingPlaylistUuidWithLoadingState 
@@ -166,7 +177,7 @@ export default function TreeGraph (
       .attr('class', styles.PlaylistTracksCountText)
       .attr('dominant-baseline', 'middle')
       .attr('text-anchor', 'middle')
-      .attr('x', GENRE_TREE_RECT_DIMENSIONS.WIDTH / 2 - 20)
+      .attr('x', GENRE_TREE_RECT_DIMENSIONS.WIDTH / 2 - PLAYLIST_TRACKS_COUNT_TEXT_OFFSET)
       .attr('y', 0)
       .text(function(d) {
         return d.data.criteriaPlaylist.libraryTracksCount;
@@ -176,19 +187,47 @@ export default function TreeGraph (
       .attr('class', styles.PlusButton)
       .attr('dominant-baseline', 'middle')
       .attr('text-anchor', 'middle')
-      .attr('x', GENRE_TREE_RECT_DIMENSIONS.WIDTH / 2 - 10)
+      .attr('x', GENRE_TREE_RECT_DIMENSIONS.WIDTH / 2 - PLUS_BUTTON_OFFSET)
       .attr('y', 0)
       .text('+')
       .on('click', function(event, d) {
         handleGenreAddClick(event, d.data.uuid);
       })
 
-    nodes.on('mouseover', function() {
-        d3.select(this).select("." + styles.PlusButton).style('display', 'block');
+    const TRACK_UPLOAD_ICON = {
+      WIDTH: 14,
+      HEIGHT: 16,
+    }
+
+    nodes.append('foreignObject')
+      .attr('class', styles.TrackUploadButtonContainer)
+      .attr('width', TRACK_UPLOAD_ICON.WIDTH)
+      .attr('height', TRACK_UPLOAD_ICON.HEIGHT)
+      .attr('dominant-baseline', 'middle')
+      .attr('x', GENRE_TREE_RECT_DIMENSIONS.WIDTH / 2 - TRACK_UPLOADED_BUTTON_OFFSET - TRACK_UPLOAD_ICON.WIDTH / 2)
+      .attr('y', - (GENRE_TREE_RECT_DIMENSIONS.HEIGHT - TRACK_UPLOAD_ICON.HEIGHT) / 2)
+      .html(function() {
+        return ReactDOMServer.renderToString(
+          <div className={styles.TrackUploadIconContainer}>
+            <FaFileUpload size={TRACK_UPLOAD_ICON.WIDTH} />
+          </div>
+      )})
+      .on('click', function(event, d) {
+        event.stopPropagation();
+        selectingFileGenreUuidRef.current = d.data.uuid;
+        fileInputRef.current.click();
       })
-      .on('mouseout', function() {
-        d3.select(this).select("." + styles.PlusButton).style('display', 'none');
-      });
+
+    nodes.on('mouseover', function() {
+      for (let className of [styles.PlusButton, styles.TrackUploadButtonContainer]) {
+        d3.select(this).select("." + className).style('display', 'block');
+      }
+    })
+    .on('mouseout', function() {
+      for (let className of [styles.PlusButton, styles.TrackUploadButtonContainer]) {
+        d3.select(this).select("." + className).style('display', 'none');
+      }
+    });
 
     return () => {
       svg.selectAll('*').remove();
@@ -196,7 +235,10 @@ export default function TreeGraph (
   }, [genres, playState, playingPlaylistUuidWithLoadingState]);
 
   return (
-    <svg ref={svgRef} width="600" height="400" className={styles.TreeGraphSvg}></svg>
+    <div>
+      <input type="file" ref={fileInputRef} style={{ display: 'none' }} onChange={handleFileChange} />
+      <svg ref={svgRef} width="600" height="400" className={styles.TreeGraphSvg}></svg>
+    </div>
   );
 }
 
