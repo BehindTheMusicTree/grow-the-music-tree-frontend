cc14bc26cc952f8a3a76771608e4cab0684838d0
mignot <andreas.garcia@bodzify.com>
2024-04-17 15:29:07 +0200

    Change output build dir to build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import eslintPlugin from 'vite-plugin-eslint' // Ajoutez cette ligne
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslintPlugin()],
+  build: {
+    outDir: 'build',
+  },
   server: {
     host: true,
     port: 5000, 
