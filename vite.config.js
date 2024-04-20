f23538b422a296d7ee62101db21448f752e9300b
mignot <andreas.garcia@bodzify.com>
2024-04-20 15:47:13 +0200

    Install sentry

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import eslintPlugin from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslintPlugin()],
+  plugins: [react(), eslintPlugin(), sentryVitePlugin({
+    org: "bodzify",
+    project: "javascript-react"
+  })],
   build: {
     outDir: 'build',
+    sourcemap: true
   },
   server: {
     host: true,
