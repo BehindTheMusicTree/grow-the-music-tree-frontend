2a5ebbf7cefffaac7c40cd13731a2ce00a64b44e
mignot <andreas.garcia@bodzify.com>
2024-04-22 14:12:58 +0200

    Make tailwind css work

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,11 +1,10 @@
-import './Banner.css'
 import Header from './Header'
 import SearchForm from './SearchForm'
 import PropTypes from 'prop-types';
 
 export default function Banner({searchSubmitted, setSearchSubmitted}) {
     return (
-    <div className='banner'>
+    <div className='banner p-10 flex flex-col'>
         <Header />
         <SearchForm setSearchSubmitted={setSearchSubmitted}/>
         <span>Search term: {searchSubmitted}</span>
