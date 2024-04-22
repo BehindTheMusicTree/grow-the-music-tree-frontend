9ebc20c3da79bce1daae5e20564283956496211f
mignot <andreas.garcia@bodzify.com>
2024-04-22 14:27:05 +0200

    Make banner use tailwind and improve design

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,13 +1,18 @@
-import Header from './Header'
-import SearchForm from './SearchForm'
 import PropTypes from 'prop-types';
+
+import SearchForm from './SearchForm'
 
 export default function Banner({searchSubmitted, setSearchSubmitted}) {
     return (
-    <div className='banner p-10 flex flex-col'>
-        <Header />
-        <SearchForm setSearchSubmitted={setSearchSubmitted}/>
-        <span>Search term: {searchSubmitted}</span>
+    <div className='banner p-10 flex flex bg-black text-gray-300'>
+        <div className='lest-banner flex flex-col justify-between w-4/5'>
+            <h1 className='text-4xl font-bold'>Ultimate Music Guide</h1>
+            <h3>by Bodzify</h3>
+        </div>
+        <div className='right-banner flex flex-col'>
+            <SearchForm setSearchSubmitted={setSearchSubmitted}/>
+            <span>Search term: {searchSubmitted}</span>
+        </div>
     </div>)
 }
 
