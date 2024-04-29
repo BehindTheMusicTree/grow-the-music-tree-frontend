e88634b1d3d3aab8f444ae32bbfa7ab185f7a7e4
mignot <andreas.garcia@bodzify.com>
2024-04-29 16:53:35 +0200

    Add logo on banner

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,15 +1,20 @@
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
 import SearchForm from './SearchForm'
+import logo from '../../assets/images/logos/umg-logo-1-200.png'
+
 
 export default function Banner({searchSubmitted, setSearchSubmitted}) {
     return (
-    <div className='p-10 flex flex bg-black text-gray-300'>
-        <div className='flex flex-col justify-between w-4/5'>
+    <div className='p-3 flex flex bg-black text-gray-300'>
+        <div className='mr-3 flex flex-col justify-center'>
+            <img src={logo} alt='logo' />
+        </div>
+        <div className='flex flex-col justify-center w-4/5'>
             <h1 className='text-4xl font-bold'>Ultimate Music Guide</h1>
             <h3>by Bodzify</h3>
         </div>
-        <div className='flex flex-col'>
+        <div className='flex flex-col justify-center'>
             <SearchForm setSearchSubmitted={setSearchSubmitted}/>
             <span>Search term: {searchSubmitted}</span>
         </div>
