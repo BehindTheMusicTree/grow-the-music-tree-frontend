6b171dd4101508525e4d6bba351d421fc198df15
mignot <andreas.garcia@bodzify.com>
2024-05-02 15:52:21 +0200

    Improve popup design

--- src/components/popup/Popup.jsx
+++ src/components/popup/Popup.jsx
@@ -9,11 +9,13 @@ export default function Popup() {
 
   return (
     <div className="popup">
-      <div className="popup-content">
-        <h2>{popupObject.title}</h2>
-        <p>{popupObject.message}</p>
-        <button onClick={hidePopup}>Fermer</button>
-      </div>
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-white p-5 rounded-lg max-w-md w-full relative">
+                <h2>{popupObject.title}</h2>
+                <p>{popupObject.message}</p>
+                <button onClick={hidePopup}>Fermer</button>
+            </div>
+        </div>
     </div>
   );
 }
