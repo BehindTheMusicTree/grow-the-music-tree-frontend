f7401b242eb78967c866f83cb8b5cf1f0a0a65fd
mignot <andreas.garcia@bodzify.com>
2024-05-07 15:06:36 +0200

    Format code

--- src/components/page-container/track-list-sidebar/track-element/TrackElement.jsx
+++ src/components/page-container/track-list-sidebar/track-element/TrackElement.jsx
@@ -2,10 +2,7 @@ import PropTypes from "prop-types";
 import { formatTime } from "../../../../utils";
 import { MdMoreVert } from "react-icons/md";
 
-export default function TrackElement({
-  playlistTrackRelationObject,
-  setEditingTrack,
-}) {
+export default function TrackElement({ playlistTrackRelationObject, setEditingTrack }) {
   const handleEditClick = (event) => {
     event.stopPropagation();
     setEditingTrack(playlistTrackRelationObject.libraryTrack);
@@ -21,18 +18,14 @@ export default function TrackElement({
           {playlistTrackRelationObject.libraryTrack.title}
         </div>
         {playlistTrackRelationObject.libraryTrack.artist ? (
-          <div className="Artist text-base text-overflow">
-            {playlistTrackRelationObject.libraryTrack.artist.name}{" "}
-          </div>
+          <div className="Artist text-base text-overflow">{playlistTrackRelationObject.libraryTrack.artist.name} </div>
         ) : (
           ""
         )}
       </div>
       <div className="AlbumGenreContainer flex flex-col items-end justify-center w-1/2 ml-2 text-xs">
         <div className="AlbumName mb-1 text-overflow">
-          {playlistTrackRelationObject.libraryTrack.album
-            ? playlistTrackRelationObject.libraryTrack.album.name
-            : ""}
+          {playlistTrackRelationObject.libraryTrack.album ? playlistTrackRelationObject.libraryTrack.album.name : ""}
         </div>
         {playlistTrackRelationObject.libraryTrack.genre ? (
           <div className="GenreNameContainer font-bold p-1 border border-gray-400 text-overflow">
@@ -45,11 +38,8 @@ export default function TrackElement({
       <div className="Duration flex text-base w-16 items-center justify-center">
         {formatTime(playlistTrackRelationObject.libraryTrack.duration)}
       </div>
-      <div
-        className="Edit flex text-base w-6 items-center justify-center"
-        onClick={handleEditClick}
-      >
-        <MdMoreVert size={18} />
+      <div className="Edit flex text-base w-6 items-center justify-center mr-2" onClick={handleEditClick}>
+        <MdMoreVert size={20} />
       </div>
     </div>
   );
