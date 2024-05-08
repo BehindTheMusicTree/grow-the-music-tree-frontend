7574ad9c5acb9f95d1d1497b902db59ac4aa0618
mignot <andreas.garcia@bodzify.com>
2024-05-08 21:43:27 +0200

    Improve track edition design

--- src/components/popup/child/LibTrackEditionPopupChild.jsx
+++ src/components/popup/child/LibTrackEditionPopupChild.jsx
@@ -24,10 +24,6 @@ export default function LibTrackEdition({ popupContentObject, hidePopup }) {
   return (
     <div className="TrackEdition w-full max-w-2xl mx-auto px-5 py-2 shadow-md">
       <form className="Form flex flex-col" onSubmit={handleSubmit(onSubmit)}>
-        <label className="popup-content-label">
-          <span>Filename:</span>
-          <div className="popup-content-input-readonly inline-block">{popupContentObject.libTrack.file.filename}</div>
-        </label>
         <div className="columns flex justify-between">
           <div className="title popup-content-column">
             <label className="popup-content-label">
@@ -42,17 +38,18 @@ export default function LibTrackEdition({ popupContentObject, hidePopup }) {
               <span>Genre:</span>
               <input className="popup-content-input" type="text" {...register("genreName")} />
             </label>
+            <label className="genre popup-content-label">
+              <span>Duration:</span>
+              <div className="popup-content-input-readonly inline-block">
+                {formatTime(popupContentObject.libTrack.duration)}
+              </div>
+            </label>
           </div>
           <div className="popup-content-column">
             <label className="popup-content-label">
+              <span>Added on:</span>
               <div className="popup-content-input-readonly inline-block mr-1">
-                {formatTime(popupContentObject.libTrack.duration)}
-              </div>
-              <div className="popup-content-input-readonly inline-block mr-1">
-                {popupContentObject.libTrack.file.sizeInMo} Mo
-              </div>
-              <div className="popup-content-input-readonly inline-block">
-                {popupContentObject.libTrack.file.bitrateInKbps} kbps
+                {new Date(popupContentObject.libTrack.addedOn).toLocaleDateString()}
               </div>
               <span className="flex-grow"></span>
             </label>
@@ -60,6 +57,19 @@ export default function LibTrackEdition({ popupContentObject, hidePopup }) {
               <span>Album:</span>
               <input className="popup-content-input" type="text" {...register("albumName")} />
             </label>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="popup-content-input-readonly inline-block mr-1">
+            {popupContentObject.libTrack.file.filename}
+          </div>
+        </div>
+        <div>
+          <div className="popup-content-input-readonly inline-block mr-1">
+            {popupContentObject.libTrack.file.sizeInMo} Mo
+          </div>
+          <div className="popup-content-input-readonly inline-block">
+            {popupContentObject.libTrack.file.bitrateInKbps} kbps
           </div>
         </div>
         <div className="flex">
