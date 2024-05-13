d2d98b6ac6cc77e0e558b4e91cb598bd39840a2e
mignot <andreas.garcia@bodzify.com>
2024-05-13 15:27:17 +0200

    Set track player object as context

--- src/components/player/Player.jsx
+++ src/components/player/Player.jsx
@@ -1,47 +1,47 @@
-import { useState, useEffect } from 'react'
-import PropTypes from 'prop-types';
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faVolumeUp, faListUl } from '@fortawesome/free-solid-svg-icons'
-import { FaPlay, FaPause, FaStepForward, FaStepBackward } from 'react-icons/fa';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeUp, faListUl } from "@fortawesome/free-solid-svg-icons";
+import { FaPlay, FaPause, FaStepForward, FaStepBackward } from "react-icons/fa";
 
-import { PLAY_STATES } from '../../constants.js';
+import { PLAY_STATES } from "../../constants.js";
+import { usePlayerTrackObject } from "../../contexts/player-track-object/usePlayerTrackObject.jsx";
 
-import albumCover from '../../assets/images/album-cover-default.png';
-import Button from '../button/Button.jsx';
+import albumCover from "../../assets/images/album-cover-default.png";
+import Button from "../button/Button.jsx";
 
-import TrackProgress from './TrackProgress/TrackProgress.jsx';
+import TrackProgress from "./TrackProgress/TrackProgress.jsx";
 
-export default function Player ({
-    playerTrackObject, 
-    playState, 
-    setPlayState, 
-    shouldResetPlayerSeek, 
-    setshouldResetPlayerSeek, 
-    setNextTrack, 
-    setPreviousTrack, 
-    setIsTrackListSidebarVisible}) {
+export default function Player({
+  playState,
+  setPlayState,
+  shouldResetPlayerSeek,
+  setshouldResetPlayerSeek,
+  setNextTrack,
+  setPreviousTrack,
+  setIsTrackListSidebarVisible,
+}) {
   const SEEK_THRESHOLD_AFTER_WHICH_TO_SKIP = 2;
 
+  const { playerTrackObject } = usePlayerTrackObject();
   const [seek, setSeek] = useState(0);
   const [volume, setVolume] = useState(0.5);
 
   const handlePlaylistIconClick = () => {
-    setIsTrackListSidebarVisible(b => !b);
-  }
-  
+    setIsTrackListSidebarVisible((b) => !b);
+  };
+
   const handlePlayPause = () => {
     if (playState === PLAY_STATES.STOPPED) {
       setshouldResetPlayerSeek(true);
       setPlayState(PLAY_STATES.PLAYING);
-    }
-    else if (playState === PLAY_STATES.PLAYING) {
+    } else if (playState === PLAY_STATES.PLAYING) {
       setPlayState(PLAY_STATES.PAUSED);
-    }
-    else {
+    } else {
       setPlayState(PLAY_STATES.PLAYING);
     }
-  }
+  };
 
   const handleVolumeChange = (event) => {
     setVolume(Number(event.target.value));
@@ -50,89 +50,86 @@ export default function Player ({
   const handleTrackEnd = () => {
     if (playerTrackObject.hasNext) {
       setNextTrack();
-    }
-    else {
+    } else {
       setPlayState(PLAY_STATES.STOPPED);
     }
-  }
+  };
 
   const handleForwardClick = () => {
     setNextTrack();
-  }
+  };
 
   const handleBackwardClick = () => {
     if (!playerTrackObject.hasPrevious || seek > SEEK_THRESHOLD_AFTER_WHICH_TO_SKIP) {
       setshouldResetPlayerSeek(true);
+    } else {
+      setPreviousTrack((prev) => prev - 1);
     }
-    else {
-      setPreviousTrack(prev => prev - 1);
-    }
-  }
+  };
 
   useEffect(() => {
     setshouldResetPlayerSeek(true);
-  }, [playerTrackObject])
+  }, [playerTrackObject]);
 
   return (
     <div className="w-full fixed left-0 bottom-0 flex justify-between p-2 bg-black text-white text-sm">
       <div className="flex-1 flex items-center justify-center">
         <img className="flex-none w-16 h-16 overflow-hidden mr-5" src={albumCover} alt="Album Cover" />
         <div className="flex-1 flex flex-col items-start justify-center w-full">
-          <div>
-            {playerTrackObject ? playerTrackObject.title : ''}
-          </div>
-          <div>
-            {playerTrackObject ? (playerTrackObject.artist ? playerTrackObject.artist.name : '')  : ''}
-          </div>
+          <div>{playerTrackObject ? playerTrackObject.title : ""}</div>
+          <div>{playerTrackObject ? (playerTrackObject.artist ? playerTrackObject.artist.name : "") : ""}</div>
         </div>
       </div>
       <div className="flex-2 flex flex-col justify-center items-center">
         <div className="flex flex-row items-center justify-center w-full">
-          <Button className="player-control-button"            
-            onClick={handleBackwardClick}>
-              <FaStepBackward />
+          <Button className="player-control-button" onClick={handleBackwardClick}>
+            <FaStepBackward />
           </Button>
-          <Button className={`player-control-button text-1.5xl py-4 mx-1
-            ${playState !== PLAY_STATES.PLAYING ? 'pl-play-l-offset pr-play-r-offset' : 'px-4'}`} 
-            onClick={handlePlayPause}>
-            <div className="text-1.5xl">
-              {playState === PLAY_STATES.PLAYING ? <FaPause /> : <FaPlay />}
-            </div>
+          <Button
+            className={`player-control-button text-1.5xl py-4 mx-1
+            ${playState !== PLAY_STATES.PLAYING ? "pl-play-l-offset pr-play-r-offset" : "px-4"}`}
+            onClick={handlePlayPause}
+          >
+            <div className="text-1.5xl">{playState === PLAY_STATES.PLAYING ? <FaPause /> : <FaPlay />}</div>
           </Button>
-          <Button className={playerTrackObject.hasNext ? "player-control-button" : "player-control-button-disabled"}
-            onClick={handleForwardClick}>
-              <FaStepForward />
+          <Button
+            className={playerTrackObject.hasNext ? "player-control-button" : "player-control-button-disabled"}
+            onClick={handleForwardClick}
+          >
+            <FaStepForward />
           </Button>
         </div>
-        <TrackProgress 
-          playState={playState} 
-          setPlayState={setPlayState} 
-          shouldResetPlayerSeek={shouldResetPlayerSeek} 
+        <TrackProgress
+          playState={playState}
+          setPlayState={setPlayState}
+          shouldResetPlayerSeek={shouldResetPlayerSeek}
           setshouldResetPlayerSeek={setshouldResetPlayerSeek}
           playerTrackObject={playerTrackObject}
           volume={volume}
           handleTrackEnd={handleTrackEnd}
           seek={seek}
-          setSeek={setSeek}/>
+          setSeek={setSeek}
+        />
       </div>
       <div className="flex-1 w-full pl-10 flex items-center justify-center">
         <div className="w-4 h-4 mr-1 transform hover:scale-120 cursor-pointer">
-          <FontAwesomeIcon icon={faListUl} onClick={handlePlaylistIconClick}/>
+          <FontAwesomeIcon icon={faListUl} onClick={handlePlaylistIconClick} />
         </div>
         <div className="flex items-center justify-center w-full">
-          <FontAwesomeIcon icon={faVolumeUp} className="w-5 h-5 mr-2"/>
-          <input className="w-24 mr-2"
-            type='range' 
-            min='0' 
-            max='1' 
-            step='.01' 
-            value={volume} 
-            onChange={handleVolumeChange} 
+          <FontAwesomeIcon icon={faVolumeUp} className="w-5 h-5 mr-2" />
+          <input
+            className="w-24 mr-2"
+            type="range"
+            min="0"
+            max="1"
+            step=".01"
+            value={volume}
+            onChange={handleVolumeChange}
           />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 Player.propTypes = {
@@ -143,5 +140,5 @@ Player.propTypes = {
   setshouldResetPlayerSeek: PropTypes.func.isRequired,
   setNextTrack: PropTypes.func.isRequired,
   setPreviousTrack: PropTypes.func.isRequired,
-  setIsTrackListSidebarVisible: PropTypes.func.isRequired
+  setIsTrackListSidebarVisible: PropTypes.func.isRequired,
 };
