ceca13f955ecf4295809352abf4b12e2469bb590
mignot <andreas.garcia@bodzify.com>
2024-05-15 20:15:00 +0200

    Display upload error in same popup as track upload

--- src/components/popup/child/LibTrackUploadingPopupChild.jsx
+++ src/components/popup/child/LibTrackUploadingPopupChild.jsx
@@ -1,19 +1,27 @@
 import PropTypes from "prop-types";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import ApiService from "../../../utils/service/apiService";
 import { useGenrePlaylists } from "../../../contexts/genre-playlists/useGenrePlaylists";
+import { BadRequestError } from "../../../utils/errors/BadRequestError";
 
 export default function LibTrackUploadingPopupChild({ popupContentObject, hidePopup }) {
   const { setRefreshGenrePlaylistsSignal } = useGenrePlaylists();
+  const [requestErrors, setRequestErrors] = useState();
   const isPostingRef = useRef(false);
 
   useEffect(() => {
     async function postLibTracks(file, genreUuid) {
-      await ApiService.postLibTracks(file, genreUuid);
+      try {
+        await ApiService.postLibTracks(file, genreUuid);
+        setRefreshGenrePlaylistsSignal(1);
+        hidePopup();
+      } catch (error) {
+        if (error instanceof BadRequestError) {
+          setRequestErrors(error.requestErrors);
+        }
+      }
       isPostingRef.current = false;
-      setRefreshGenrePlaylistsSignal(1);
-      hidePopup();
     }
 
     if (popupContentObject && !isPostingRef.current) {
@@ -21,7 +29,30 @@ export default function LibTrackUploadingPopupChild({ popupContentObject, hidePo
       postLibTracks(popupContentObject.file, popupContentObject.genreUuid);
     }
   }, [popupContentObject]);
-  return <div>Uploading...</div>;
+
+  return (
+    <div>
+      {requestErrors ? (
+        <div>
+          {requestErrors &&
+            requestErrors.map((operationError) =>
+              Object.entries(operationError).map(([fieldName, fieldErrors]) => (
+                <div key={fieldName}>
+                  <h3>{fieldName}</h3>
+                  <ul>
+                    {fieldErrors.map((error) => (
+                      <li key={error}>{error}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))
+            )}
+        </div>
+      ) : (
+        <h3>Uploading...</h3>
+      )}
+    </div>
+  );
 }
 
 LibTrackUploadingPopupChild.propTypes = {
