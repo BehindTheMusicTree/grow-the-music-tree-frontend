41bf801205da27ca3e2b398068a28d4c8e6d946e
mignot <andreas.garcia@bodzify.com>
2024-05-16 18:47:38 +0200

    Set a min height for track list sidebar

--- src/components/page-container/PageContainer.jsx
+++ src/components/page-container/PageContainer.jsx
@@ -19,8 +19,6 @@ export default function PageContainer({ pageTypeWithObject }) {
         ")] flex flex-col bg-gray-200 m-0 px-8 pb-5"
       }
     >
-      {/* <div className="page-container flex-grow overflow-auto max-h-[calc(100%-180px)] flex flex-col bg-gray-200 m-0 px-8 pb-5"> */}
-
       {pageTypeWithObject.current.type === CONTENT_AREA_TYPES.GENRES ? (
         <GenresPage />
       ) : (
@@ -31,7 +29,7 @@ export default function PageContainer({ pageTypeWithObject }) {
       : <IconPause dataTestId="pause"/>
         } */}
       {isTrackListSidebarVisible ? (
-        <div className="track-list-sidebar-container absolute right-0 w-144 overflow-auto max-h-[calc(100%-180px)] rounded-2xl bg-gray-950">
+        <div className="track-list-sidebar-container absolute bottom-0 right-0 w-144 overflow-auto max-h-[calc(100%-180px)] min-h-[calc((100%-180px)/3)] rounded-2xl bg-gray-950">
           <TrackListSidebar />
         </div>
       ) : null}
