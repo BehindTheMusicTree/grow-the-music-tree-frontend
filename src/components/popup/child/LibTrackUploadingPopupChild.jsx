19c83350c3b6b9b3b3ee6d4e2fc65e859f738b41
mignot <andreas.garcia@bodzify.com>
2024-05-16 09:17:12 +0200

    Improve track upload error display

--- src/components/popup/child/LibTrackUploadingPopupChild.jsx
+++ src/components/popup/child/LibTrackUploadingPopupChild.jsx
@@ -1,6 +1,8 @@
 import PropTypes from "prop-types";
 import { useEffect, useRef, useState } from "react";
 
+import { MdError } from "react-icons/md";
+
 import ApiService from "../../../utils/service/apiService";
 import { useGenrePlaylists } from "../../../contexts/genre-playlists/useGenrePlaylists";
 import { BadRequestError } from "../../../utils/errors/BadRequestError";
@@ -28,20 +30,24 @@ export default function LibTrackUploadingPopupChild({ popupContentObject, hidePo
       isPostingRef.current = true;
       postLibTracks(popupContentObject.file, popupContentObject.genreUuid);
     }
-  }, []);
+  }, [popupContentObject]);
 
   return (
     <div>
       {requestErrors ? (
         <div>
+          <div className="flex items-center">
+            <MdError size={20} color="red" />
+            <h3 className="ml-1">An error occured</h3>
+          </div>
           {requestErrors &&
             requestErrors.map((operationError) =>
               Object.entries(operationError).map(([fieldName, fieldErrors]) => (
-                <div key={fieldName}>
-                  <h3>{fieldName}</h3>
+                <div key={fieldName} className="flex">
+                  <h3 className="mr-2">{fieldName}</h3>
                   <ul>
                     {fieldErrors.map((error) => (
-                      <li key={error}>{error}</li>
+                      <li key={error}>- {error}</li>
                     ))}
                   </ul>
                 </div>
