6f05465d63703a744a2bee9342e65d7872eb2550
mignot <andreas.garcia@bodzify.com>
2024-05-16 14:13:07 +0200

    Make body takes available height when no player

--- src/components/page-container/PageContainer.jsx
+++ src/components/page-container/PageContainer.jsx
@@ -3,14 +3,24 @@ import PropTypes from "prop-types";
 import TrackListSidebar from "./track-list-sidebar/TrackListSidebar";
 import { CONTENT_AREA_TYPES } from "../../constants";
 import GenresPage from "./genres-page/GenresPage";
-import { useTrackListSidebarVisibility } from "../../contexts/track-list-sidebar-visibility/useTrackListSidebarVisibility.jsx";
+import { useTrackListSidebarVisibility } from "../../contexts/track-list-sidebar-visibility/useTrackListSidebarVisibility";
+import { usePlayerTrackObject } from "../../contexts/player-track-object/usePlayerTrackObject";
 
 export default function PageContainer({ pageTypeWithObject }) {
   const { isTrackListSidebarVisible } = useTrackListSidebarVisibility();
+  const { playerTrackObject } = usePlayerTrackObject();
 
   return (
-    /* 180px being the sum of the banner and player heights */
-    <div className="page-container flex-grow overflow-auto max-h-[calc(100%-180px)] flex flex-col bg-gray-200 m-0 px-8 pb-5">
+    /* 180px being the sum of the banner and player heights, 100px being the height of the banner alone */
+    <div
+      className={
+        "page-container flex-grow overflow-auto max-h-[calc(100%-" +
+        (playerTrackObject ? "180px" : "100px") +
+        ")] flex flex-col bg-gray-200 m-0 px-8 pb-5"
+      }
+    >
+      {/* <div className="page-container flex-grow overflow-auto max-h-[calc(100%-180px)] flex flex-col bg-gray-200 m-0 px-8 pb-5"> */}
+
       {pageTypeWithObject.current.type === CONTENT_AREA_TYPES.GENRES ? (
         <GenresPage />
       ) : (
