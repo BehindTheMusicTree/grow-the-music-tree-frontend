779e85a1a00d0382a5d55098bca63a6940b5857a
mignot <andreas.garcia@bodzify.com>
2024-05-21 16:14:27 +0200

    Reduce title column in library

--- src/components/page-container/pages/library/Library.jsx
+++ src/components/page-container/pages/library/Library.jsx
@@ -1,6 +1,7 @@
 import { useLibTracks } from "../../../../contexts/lib-tracks/useLibTracks";
 import { formatTime } from "../../../../utils";
 import { FaRegClock } from "react-icons/fa";
+import Rating from "../../../utils/Rating";
 
 export default function Library() {
   const { libTracks } = useLibTracks();
@@ -28,18 +29,25 @@ export default function Library() {
           </tr>
         </thead>
         <tbody className="space-y-10">
-          {libTracks?.map((track, index) => (
-            <tr key={track.id} className="hover:bg-gray-300">
+          {libTracks?.map((libTrack, index) => (
+            <tr key={libTrack.id} className="hover:bg-gray-300">
               <td className="library-item pl-2">{index + 1}</td>
-              <td className="library-item">{track.title}</td>
-              <td className="library-item">{track.artist ? track.artist.name : ""}</td>
-              <td className="library-item">{track.album ? track.album.name : ""}</td>
-              <td className="library-item">{track.genre ? track.genre.name : ""}</td>
-              <td className="library-item text-center">{track.rating}</td>
-              <td className="library-item">{formatTime(track.duration)}</td>
-              <td className="library-item text-center">{track.file.extension.replace(".", "")}</td>
-              <td className="library-item text-center">{track.file.bitrateInKbps} kbps</td>
-              <td className="library-item text-center">{track.playCount}</td>
+              <td className="library-item max-w-64">{libTrack.title}</td>
+              <td className="library-item">{libTrack.artist ? libTrack.artist.name : ""}</td>
+              <td className="library-item">{libTrack.album ? libTrack.album.name : ""}</td>
+              <td className="library-item">{libTrack.genre ? libTrack.genre.name : ""}</td>
+              <td className="library-item text-center">
+                <Rating
+                  rating={libTrack.rating}
+                  handleChange={() => {
+                    return;
+                  }}
+                />
+              </td>
+              <td className="library-item">{formatTime(libTrack.duration)}</td>
+              <td className="library-item text-center">{libTrack.file.extension.replace(".", "")}</td>
+              <td className="library-item text-center">{libTrack.file.bitrateInKbps} kbps</td>
+              <td className="library-item text-center">{libTrack.playCount}</td>
             </tr>
           ))}
         </tbody>
