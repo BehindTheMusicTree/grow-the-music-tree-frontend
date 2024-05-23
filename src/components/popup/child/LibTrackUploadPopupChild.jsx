e0448cafcb2fa37c6a7faf0e848eb1e891fea8e7
mignot <andreas.garcia@bodzify.com>
2024-05-23 17:59:40 +0200

    Improve design when uploading tracks

--- src/components/popup/child/LibTrackUploadPopupChild.jsx
+++ src/components/popup/child/LibTrackUploadPopupChild.jsx
@@ -1,19 +1,24 @@
 import PropTypes from "prop-types";
 import { useEffect, useState } from "react";
 
-import { MdError } from "react-icons/md";
+import { MdError, MdCheckCircle } from "react-icons/md";
 
 import { useLibTracks } from "../../../contexts/lib-tracks/useLibTracks";
 import { BadRequestError } from "../../../utils/errors/BadRequestError";
 
 export default function LibTrackUploadPopupChild({ popupContentObject }) {
   const { postLibTrack } = useLibTracks();
-  const [requestErrorsByFilename, setRequestErrorsByFilename] = useState([]);
   const [isPosting, setIsPosting] = useState(false);
-  const [uploadProgress, setUploadProgress] = useState({});
+  const [filesUploadObjs, setFilesUploadObjs] = useState({});
 
   useEffect(() => {
     if (popupContentObject && !isPosting) {
+      popupContentObject.files.forEach((file) => {
+        setFilesUploadObjs((prevUploadObj) => ({
+          ...prevUploadObj,
+          [file.name]: { size: file.size, progress: 0, isPosting: false, requestErrors: {} },
+        }));
+      });
       setIsPosting(true);
     }
   }, [popupContentObject]);
@@ -21,18 +26,18 @@ export default function LibTrackUploadPopupChild({ popupContentObject }) {
   useEffect(() => {
     async function handleLibTrackToPost(file, genreUuid) {
       try {
-        await postLibTrack(file, genreUuid, (progress) =>
-          setUploadProgress((prevProgress) => ({
-            ...prevProgress,
-            [file.name]: { size: file.size, progress: progress },
-          }))
-        );
+        await postLibTrack(file, genreUuid, (progress) => {
+          setFilesUploadObjs((prevFilesUploadObjs) => ({
+            ...prevFilesUploadObjs,
+            [file.name]: { ...prevFilesUploadObjs[file.name], progress },
+          }));
+        });
       } catch (error) {
         if (error instanceof BadRequestError) {
-          setRequestErrorsByFilename((requestErrorsByFilename) => [
-            ...requestErrorsByFilename,
-            { [file.name]: error.requestErrors },
-          ]);
+          setFilesUploadObjs((fileUploadObj) => ({
+            ...fileUploadObj,
+            [file.name]: { ...fileUploadObj[file.name], requestErrors: error.requestErrors },
+          }));
         }
       }
     }
@@ -50,24 +55,49 @@ export default function LibTrackUploadPopupChild({ popupContentObject }) {
 
   return (
     <div>
-      {isPosting ? (
+      <div>
         <div>
-          <div>
-            {Object.entries(uploadProgress).map(([filename, { size, progress }]) => (
-              <div key={filename} className="flex items-center">
-                <div className="w-1/2 mr-4">{filename}</div>
-                <div className="flex-grow h-4 bg-gray-200 rounded-md overflow-hidden mr-4 relative">
-                  <div className="bg-blue-500 h-full" style={{ width: `${progress}%` }} />
-                  <div className="absolute inset-0 flex items-center justify-center text-sm text-white">
-                    {Math.round(progress)}%
-                  </div>
+          {Object.entries(filesUploadObjs).map(([filename, uploadObj]) => {
+            console.log(JSON.stringify(uploadObj, null, 2));
+            return (
+              <div key={filename} className="h-8 flex items-center">
+                {uploadObj.requestErrors.length > 0 ? (
+                  <MdError size={20} color="red" className="mr-1" />
+                ) : (
+                  <MdCheckCircle size={20} color="green" className="mr-1" />
+                )}
+                <div className="w-2/5 mr-4 text-overflow">{filename}</div>
+                <div className="center flex-grow mr-2">
+                  {uploadObj.requestErrors.length > 0 ? (
+                    <div>
+                      {uploadObj.requestErrors.map((fieldsErrors, errorIndex) => {
+                        return Object.entries(fieldsErrors).map(([fieldName, fieldErrors], index) => {
+                          return fieldErrors.map((fieldError, subIndex) => {
+                            return (
+                              <div key={`${fieldName}-${errorIndex}-${index}-${subIndex}`} className="text-left">
+                                {fieldError}
+                              </div>
+                            );
+                          });
+                        });
+                      })}
+                    </div>
+                  ) : (
+                    <div className="h-4 bg-gray-200 rounded-md overflow-hidden mr-4 relative">
+                      <div className="bg-blue-500 h-full" style={{ width: `${uploadObj.progress}%` }} />
+                      <div className="absolute inset-0 flex items-center justify-center text-sm text-white">
+                        {Math.round(uploadObj.progress)}%
+                      </div>
+                    </div>
+                  )}
                 </div>
-                <div className="w-16">{(size / 1048576).toFixed(2)} Mo</div>{" "}
+                <div className="w-16">{(uploadObj.size / 1048576).toFixed(2)} Mo</div>
               </div>
-            ))}
-          </div>
+            );
+          })}
         </div>
-      ) : requestErrorsByFilename.length > 0 ? (
+      </div>
+      {/* requestErrorsByFilename.length > 0 ? (
         <div>
           {requestErrorsByFilename.map((errorObj) => {
             const filename = Object.keys(errorObj)[0];
@@ -98,7 +128,7 @@ export default function LibTrackUploadPopupChild({ popupContentObject }) {
             );
           })}
         </div>
-      ) : null}
+      ) : null} */}
     </div>
   );
 }
