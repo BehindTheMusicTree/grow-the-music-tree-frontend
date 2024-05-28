c0299b61c77176617f8dab074ac7cb9aff43cb32
mignot <andreas.garcia@bodzify.com>
2024-05-28 16:04:31 +0200

    Insert a link on mail contact message

--- src/models/popup-content-object/ApiErrorPopupContentObject.js
+++ src/models/popup-content-object/ApiErrorPopupContentObject.js
@@ -5,7 +5,8 @@ export default class ApiErrorPopupContentObject extends PopupContentObject {
   constructor() {
     super("An error occurred");
     this.message = `We are sorry, an error occurred. Please try again later.<br/>
-    If the problem persists, please contact us at the following address: ${config.contactEmail}.<br/>
-    If the problem still persists, pray for God or Willie Nelson.`;
+    If the problem persists, please contact us at the following address: 
+    <a href="mailto:${config.contactEmail}">${config.contactEmail}</a>.<br/>
+    If the problem persist agayyn, pray for Willie Nelson.`;
   }
 }
