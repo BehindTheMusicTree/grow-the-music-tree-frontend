6000b885e8bb0b463514e5e63c3b36244feb18ee
mignot <andreas.garcia@bodzify.com>
2024-09-25 17:31:12 +0200

    Remove search submitted test

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import SearchForm from "./SearchForm";
 import logo from "../../assets/images/logos/umg-logo-maze-100.svg";
 
-export default function Banner({ searchSubmitted, setSearchSubmitted }) {
+export default function Banner({ setSearchSubmitted }) {
   return (
     <div className="h-banner p-3 flex bg-black text-gray-100">
       <div className="mr-3 flex flex-col justify-center">
@@ -16,7 +16,6 @@ export default function Banner({ searchSubmitted, setSearchSubmitted }) {
       <div className="filler flex-grow" />
       <div className="search w-64 flex flex-col justify-center">
         <SearchForm setSearchSubmitted={setSearchSubmitted} />
-        <span>Search term: {searchSubmitted}</span>
       </div>
     </div>
   );
