55408fbc5dfe8cb3ed6491c0fd1c6185dd5241d8
mignot <andreas.garcia@bodzify.com>
2025-04-09 19:03:18 +0200

    Implement PageContext to synchronize page state with route changes using useEffect

--- src/contexts/PageContext.jsx
+++ src/contexts/PageContext.jsx
@@ -1,5 +1,8 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
+import Page from "@models/Page";
+import { PAGE_TYPES } from "@utils/constants";
 
 export const PageContext = createContext();
 
@@ -12,13 +15,37 @@ export function usePage() {
 }
 
 export function PageProvider({ children }) {
-  const [currentPage, setCurrentPage] = useState("home");
+  const [page, setPage] = useState(new Page(PAGE_TYPES.GENRE_PLAYLISTS));
+  const location = useLocation();
+
+  // Sync route to page state
+  useEffect(() => {
+    // Synchronize route path with page context
+    switch (location.pathname) {
+      case "/":
+      case "/genre-playlists":
+        setPage(new Page(PAGE_TYPES.GENRE_PLAYLISTS));
+        break;
+      case "/uploaded-library":
+        setPage(new Page(PAGE_TYPES.UPLOADED_LIBRARY));
+        break;
+      case "/spotify-library":
+        setPage(new Page(PAGE_TYPES.SPOTIFY_LIBRARY));
+        break;
+      case "/account":
+        setPage(new Page(PAGE_TYPES.ACCOUNT));
+        break;
+      default:
+        // Handle other routes if needed
+        break;
+    }
+  }, [location.pathname]);
 
   return (
     <PageContext.Provider
       value={{
-        currentPage,
-        setCurrentPage,
+        page,
+        setPage,
       }}
     >
       {children}
