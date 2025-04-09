6faa9f4758ac17ae7cae22e622868f8cad2fbb82
mignot <andreas.garcia@bodzify.com>
2025-04-09 17:19:29 +0200

    Add path aliases for pages, routes, and layouts in vite.config.js for improved module resolution.

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,9 @@ export default defineConfig({
       "@hooks": path.resolve(__dirname, "./src/hooks"),
       "@models": path.resolve(__dirname, "./src/models"),
       "@assets": path.resolve(__dirname, "./src/assets"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@routes": path.resolve(__dirname, "./src/routes"),
+      "@layouts": path.resolve(__dirname, "./src/layouts"),
     },
   },
   plugins: [
