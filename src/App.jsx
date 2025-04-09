14e6926d95c7619fa84f63e4ae76179f0d955ade
mignot <andreas.garcia@bodzify.com>
2025-04-09 16:46:27 +0200

    Refactor App component to use AppLayout and AppRoutes, removing unnecessary context providers for a cleaner structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AppLayout } from "@layouts/AppLayout";
+import { AppRoutes } from "@routes/AppRoutes";
 
-import { AuthProvider } from "@contexts/AuthContext";
-import { PopupProvider } from "@contexts/PopupContext";
-import { UploadedTrackProvider } from "@contexts/UploadedTrackContext";
-import { GenrePlaylistProvider } from "@contexts/GenrePlaylistContext";
-import { ApiAuthenticatedProvider } from "@contexts/ApiAuthenticatedContext";
-
-import AppRoutes from "./AppRoutes";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5, // 5 minutes
-      cacheTime: 1000 * 60 * 30, // 30 minutes
-    },
-  },
-});
-
-function App() {
+export function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <AuthProvider>
-          <PopupProvider>
-            <ApiAuthenticatedProvider contextName="app">
-              <UploadedTrackProvider>
-                <GenrePlaylistProvider>
-                  <AppRoutes />
-                </GenrePlaylistProvider>
-              </UploadedTrackProvider>
-            </ApiAuthenticatedProvider>
-          </PopupProvider>
-        </AuthProvider>
-      </BrowserRouter>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <AppLayout>
+        <AppRoutes />
+      </AppLayout>
+    </BrowserRouter>
   );
 }
-
-export default App;
