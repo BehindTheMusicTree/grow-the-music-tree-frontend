ab70c99159511c04305a6da794dda676c2267591
mignot <andreas.garcia@bodzify.com>
2025-04-09 19:03:45 +0200

    Update AppRoutes to redirect from root to genre playlists and set other routes to null for future implementation.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,15 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import NotFoundPage from "@components/utils/NotFoundPage";
 import SpotifyCallback from "@components/auth/SpotifyCallback";
-import { AppLayout } from "@layouts/AppLayout";
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />} />
-      <Route path="/genre-playlists" element={<AppLayout />} />
-      <Route path="/uploaded-library" element={<AppLayout />} />
-      <Route path="/spotify-library" element={<AppLayout />} />
+      <Route path="/" element={<Navigate to="/genre-playlists" />} />
+      <Route path="/genre-playlists" element={null} />
+      <Route path="/uploaded-library" element={null} />
+      <Route path="/spotify-library" element={null} />
+      <Route path="/account" element={null} />
       <Route path="/auth/spotify/callback" element={<SpotifyCallback />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
