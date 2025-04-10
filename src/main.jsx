9f79bc4792ae51f259849976bfbeb280c8cdcd55
mignot <andreas.garcia@bodzify.com>
2025-04-10 10:53:37 +0200

    Refactor Sentry configuration to use Next.js environment variables

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { checkRequiredConfigVars } from "./utils/config";
 
 checkRequiredConfigVars();
 
-if (import.meta.env.VITE_SENTRY_IS_ACTIVE == true) {
+if (process.env.NEXT_PUBLIC_SENTRY_IS_ACTIVE === "true") {
   Sentry.init({
     dsn: "https://7f17fcd9feebfb634ad7ba2f638ba69a@o4507119053832192.ingest.de.sentry.io/4507119058026576",
     integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
@@ -17,15 +17,15 @@ if (import.meta.env.VITE_SENTRY_IS_ACTIVE == true) {
     // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
     tracePropagationTargets: [
       "localhost",
-      new RegExp(`^${import.meta.env.VITE_API_BASE_URL.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}`),
+      new RegExp(`^${process.env.NEXT_PUBLIC_API_BASE_URL.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")}`),
     ],
     // Session Replay
     replaysSessionSampleRate: 1.0, // Set sample at a lower rate in production (e.g. 0.1 for 10% of sessions)
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
     // Development server configuration
-    environment: import.meta.env.VITE_ENV,
+    environment: process.env.NODE_ENV,
     // Disable Sentry in development
-    enabled: import.meta.env.DEV ? false : import.meta.env.VITE_SENTRY_IS_ACTIVE === true,
+    enabled: process.env.NODE_ENV === "development" ? false : process.env.NEXT_PUBLIC_SENTRY_IS_ACTIVE === "true",
   });
 }
 
