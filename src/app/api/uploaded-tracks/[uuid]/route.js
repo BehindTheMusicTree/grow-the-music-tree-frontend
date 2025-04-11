e00d22a5e6cefe07731e62eeeec408b4872a415a
mignot <andreas.garcia@bodzify.com>
2025-04-11 15:29:38 +0200

    refactor: remove unused request parameter from GET function in uploaded tracks route

--- src/app/api/uploaded-tracks/[uuid]/route.js
+++ src/app/api/uploaded-tracks/[uuid]/route.js
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@lib/auth";
 
-export async function GET(request, { params }) {
+export async function GET({ params }) {
   const session = await getServerSession(authOptions);
   if (!session) {
     return Response.json({ error: "Unauthorized" }, { status: 401 });
