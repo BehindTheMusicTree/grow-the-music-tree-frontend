21155f3d24efd330634bbe470093022ddd661e95
mignot <andreas.garcia@bodzify.com>
2025-04-11 13:51:31 +0200

    refactor: add AuthProvider to the Providers component and reorganize JSX structure

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -7,21 +7,24 @@ import { UploadedTrackProvider } from "@contexts/UploadedTrackContext";
 import { GenrePlaylistProvider } from "@contexts/GenrePlaylistContext";
 import { PlayerProvider } from "@contexts/PlayerContext";
 import { TrackListSidebarVisibilityProvider } from "@contexts/TrackListSidebarVisibilityContext";
+import { AuthProvider } from "@contexts/AuthContext";
 
 const queryClient = new QueryClient();
 
 export default function Providers({ children }) {
   return (
     <QueryClientProvider client={queryClient}>
-      <PopupProvider>
-        <UploadedTrackProvider>
-          <GenrePlaylistProvider>
-            <PlayerProvider>
-              <TrackListSidebarVisibilityProvider>{children}</TrackListSidebarVisibilityProvider>
-            </PlayerProvider>
-          </GenrePlaylistProvider>
-        </UploadedTrackProvider>
-      </PopupProvider>
+      <AuthProvider>
+        <PopupProvider>
+          <UploadedTrackProvider>
+            <GenrePlaylistProvider>
+              <PlayerProvider>
+                <TrackListSidebarVisibilityProvider>{children}</TrackListSidebarVisibilityProvider>
+              </PlayerProvider>
+            </GenrePlaylistProvider>
+          </UploadedTrackProvider>
+        </PopupProvider>
+      </AuthProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
