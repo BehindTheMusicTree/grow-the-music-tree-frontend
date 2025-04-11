1a29ce3edc33a6b535045e723a4624b23acc6eca
mignot <andreas.garcia@bodzify.com>
2025-04-11 12:42:40 +0200

    refactor: update import path for constants to use alias

--- src/components/client/features/Rating.jsx
+++ src/components/client/features/Rating.jsx
@@ -2,9 +2,9 @@
 
 import { v4 as uuidv4 } from "uuid";
 import PropTypes from "prop-types";
-
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
-import { FORM_RATING_NULL_VALUE } from "../../utils/constants";
+
+import { FORM_RATING_NULL_VALUE } from "@utils/constants";
 
 export default function Rating({ rating, handleChange }) {
   const instanceId = uuidv4();
