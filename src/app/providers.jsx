6e2ee1cac58bcbd68ad6c9ab5428f4e482dc2159
mignot <andreas.garcia@bodzify.com>
2025-04-11 17:11:07 +0200

    feat: wrap Providers component with SessionProvider for authentication context

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { SessionProvider } from "next-auth/react";
 import { PopupProvider } from "@contexts/PopupContext";
 import { UploadedTrackProvider } from "@contexts/UploadedTrackContext";
 import { GenrePlaylistProvider } from "@contexts/GenrePlaylistContext";
@@ -14,21 +15,23 @@ const queryClient = new QueryClient();
 
 export default function Providers({ children }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <PopupProvider>
-        <UploadedTrackProvider>
-          <GenrePlaylistProvider>
-            <PlayerProvider>
-              <TrackListProvider>
-                <SpotifyLibraryProvider>
-                  <TrackListSidebarVisibilityProvider>{children}</TrackListSidebarVisibilityProvider>
-                </SpotifyLibraryProvider>
-              </TrackListProvider>
-            </PlayerProvider>
-          </GenrePlaylistProvider>
-        </UploadedTrackProvider>
-      </PopupProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <PopupProvider>
+          <UploadedTrackProvider>
+            <GenrePlaylistProvider>
+              <PlayerProvider>
+                <TrackListProvider>
+                  <SpotifyLibraryProvider>
+                    <TrackListSidebarVisibilityProvider>{children}</TrackListSidebarVisibilityProvider>
+                  </SpotifyLibraryProvider>
+                </TrackListProvider>
+              </PlayerProvider>
+            </GenrePlaylistProvider>
+          </UploadedTrackProvider>
+        </PopupProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </SessionProvider>
   );
 }
