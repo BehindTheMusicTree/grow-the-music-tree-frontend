34a022c7ecff6223cbba9426857834e43b170120
mignot <andreas.garcia@bodzify.com>
2025-04-17 00:02:17 +0200

    chore: remove public/env-config.js from .gitignore and add new env-config.js file

---
+++ public/env-config.js
@@ -0,0 +1,9 @@
+window._env_ = {
+  VITE_ENV: "development",
+  VITE_CONTACT_EMAIL: "garcia.andreas.1991@gmail.com",
+  VITE_API_BASE_URL: "http://localhost:8000/api/v0.2.0/",
+  VITE_SENTRY_IS_ACTIVE: "false",
+  VITE_SPOTIFY_CLIENT_ID: "4d242a80692a4a4da1243bf012820e16",
+  VITE_SPOTIFY_REDIRECT_URI: "http://127.0.0.1:5000/auth/spotify/callback",
+  VITE_SPOTIFY_SCOPE: "user-read-email playlist-read-private playlist-read-collaborative user-library-read user-top-read"
+};
