93da472592b8a6e4035b30b0a4ea79f19844e83f
mignot <andreas.garcia@bodzify.com>
2025-04-16 15:51:27 +0200

    style: update text color and remove unnecessary emoji in NotFoundPage component

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,8 +1,8 @@
 const NotFoundPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold mb-4">404</h1>
-      <p className="text-xl text-gray-600">The requested page does not exist :O</p>
+      <h1 className="text-4xl text-gray-800 font-bold mb-4">404</h1>
+      <p className="text-xl text-gray-600">The requested page does not exist</p>
     </div>
   );
 };
