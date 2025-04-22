c0a3c745f5b5d5039467a3f22ca2dad0b88468f8
mignot <andreas.garcia@bodzify.com>
2025-04-22 17:50:56 +0200

    chore: update Next.js configuration to use turbopack and adjust dev script

--- next.config.js
+++ next.config.js
@@ -2,25 +2,19 @@
 
 const nextConfig = {
   reactStrictMode: false,
-  // Configure webpack to handle the special imports and disable minimization
-  webpack: (config) => {
-    config.optimization.minimize = true;
-
-    // Configure aliases
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@app": "/src/app",
-      "@actions": "/src/app/actions",
-      "@components": "/src/components",
-      "@contexts": "/src/contexts",
-      "@layouts": "/src/layouts",
-      "@models": "/src/models",
-      "@hooks": "/src/hooks",
-      "@assets": "/src/assets",
-      "@lib": "/src/lib",
-      "@utils": "/src/lib/utils",
-    };
-    return config;
+  turbopack: {
+    resolveAlias: {
+      "@app": "./src/app",
+      "@actions": "./src/app/actions",
+      "@components": "./src/components",
+      "@contexts": "./src/contexts",
+      "@layouts": "./src/layouts",
+      "@models": "./src/models",
+      "@hooks": "./src/hooks",
+      "@assets": "./src/assets",
+      "@lib": "./src/lib",
+      "@utils": "./src/lib/utils",
+    },
   },
 };
 
