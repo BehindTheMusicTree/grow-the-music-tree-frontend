1baf7ac02e5301013039076b14e106d610364329
mignot <andreas.garcia@bodzify.com>
2025-04-22 16:35:35 +0200

    fix: enable Spotify library tracks query based on session state

--- src/contexts/SpotifyLibTracksContext.jsx
+++ src/contexts/SpotifyLibTracksContext.jsx
@@ -2,8 +2,10 @@
 
 import { createContext, useContext } from "react";
 import { useQuery } from "@tanstack/react-query";
+
 import { useAuthenticatedApi } from "@hooks/useAuthenticatedApi";
 import { listSpotifyLibTracks } from "@lib/music-tree-api-service/spotify-lib-track";
+import { useSession } from "@contexts/SessionContext";
 
 const SpotifyLibTracksContext = createContext();
 
@@ -17,7 +19,7 @@ export const useSpotifyLibTracks = () => {
 
 export const SpotifyLibTracksProvider = ({ children }) => {
   const authenticatedApi = useAuthenticatedApi(listSpotifyLibTracks);
-
+  const { session, isLoading: isSessionLoading } = useSession();
   const {
     data: spotifyLibTracks,
     loading,
@@ -32,6 +34,7 @@ export const SpotifyLibTracksProvider = ({ children }) => {
       console.log("spotifyLibTracks", result.data);
       return result.data.results;
     },
+    enabled: !isSessionLoading && !!session,
   });
 
   const value = {
