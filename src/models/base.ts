b8b5ac1dea0fdffe9caa1e280ff9857e8c8ffb93
mignot <andreas.garcia@bodzify.com>
2025-04-23 15:58:27 +0200

    feat: add UuidResource and SpotifyResource interfaces in base.ts

---
+++ src/models/base.ts
@@ -0,0 +1,11 @@
+export interface UuidResource {
+  id: string;
+  createdOn: string;
+  updatedOn: string;
+}
+
+export interface SpotifyResource {
+  spotifyId: string;
+  spotifyUri: string;
+  spotifyUrl: string;
+}
