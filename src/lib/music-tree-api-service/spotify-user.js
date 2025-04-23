a7f610a861cc9d755fb2900582541e5fe8817be5
mignot <andreas.garcia@bodzify.com>
2025-04-23 18:18:55 +0200

    chore: remove Spotify authentication and user retrieval files

--- src/lib/music-tree-api-service/spotify-user.js
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export async function retrieveSpotifyUser() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}user/spotify/`, {
-    method: "GET",
-  });
-
-  return response;
-}
