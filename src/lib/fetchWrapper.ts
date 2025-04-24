53289a335b6efff8ed9bc4d7a6a116c2741b4762
mignot <andreas.garcia@bodzify.com>
2025-04-24 15:27:50 +0200

    feat: enhance fetchWrapper to support query parameters in requests

--- src/lib/fetchWrapper.ts
+++ src/lib/fetchWrapper.ts
@@ -1,5 +1,16 @@
-export const fetchWrapper = async <T>(url: string, options: RequestInit = {}, accessToken?: string): Promise<T> => {
-  const res = await fetch(url, {
+export const fetchWrapper = async <T>(
+  url: string,
+  options: RequestInit = {},
+  accessToken?: string,
+  queryParams?: Record<string, string | number | boolean>
+): Promise<T> => {
+  const urlWithParams = queryParams
+    ? `${url}${url.includes("?") ? "&" : "?"}${new URLSearchParams(
+        Object.entries(queryParams).map(([key, value]) => [key, String(value)])
+      ).toString()}`
+    : url;
+
+  const res = await fetch(urlWithParams, {
     ...options,
     headers: {
       "Content-Type": "application/json",
