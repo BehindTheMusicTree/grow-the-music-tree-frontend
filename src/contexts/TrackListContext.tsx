d312f6e9305d0fa22fd45d72d4613790e04e5491
mignot <andreas.garcia@bodzify.com>
2025-04-25 01:20:49 +0200

    fix: update import statement for Track model in TrackListContext to use uploaded-track

--- src/contexts/TrackListContext.tsx
+++ src/contexts/TrackListContext.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { createContext, useState, useContext, ReactNode } from "react";
-import Track from "@/models/domain/spotify-lib-track";
+import Track from "@/models/domain/uploaded-track";
 
 interface TrackListContextType {
   trackList: Track[];
