db7fd6d293edba4e8e1331694a9d8b2b6928a1d1
mignot <andreas.garcia@bodzify.com>
2025-04-25 01:34:40 +0200

    fix: update import for MbRecordingDetailedSchema in FileDetailedSchema

--- src/models/domain/file.ts
+++ src/models/domain/file.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { MusicbrainzRecordingDetailedSchema } from "./mb-recording";
+import { MbRecordingDetailedSchema } from "./mb-recording";
 
 export const FileDetailedSchema = z.object({
   filename: z.string(),
@@ -12,7 +12,7 @@ export const FileDetailedSchema = z.object({
   sizeInKo: z.number(),
   sizeInMo: z.number(),
   bitrateInKbps: z.number(),
-  musicbrainzRecording: MusicbrainzRecordingDetailedSchema,
+  musicbrainzRecording: MbRecordingDetailedSchema,
   musicbrainzRecordingMissingCause: z.string().optional(),
   createdOn: z.string().datetime(),
   updatedOn: z.string().datetime(),
