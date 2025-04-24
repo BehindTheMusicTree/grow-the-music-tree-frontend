ef426355bd230be7dc8f5ecd8393750e4996f2d8
mignot <andreas.garcia@bodzify.com>
2025-04-25 00:27:59 +0200

    refactor: correct Button import path in FormPopup component

--- src/components/ui/popup/child/FormPopup.tsx
+++ src/components/ui/popup/child/FormPopup.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import { BasePopup, BasePopupProps } from "../BasePopup";
 
 interface FormPopupProps extends BasePopupProps {
