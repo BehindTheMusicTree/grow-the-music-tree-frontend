e81aafb48a41133835d0c9c90700eb4e5f3df29d
mignot <andreas.garcia@bodzify.com>
2025-04-24 15:58:13 +0200

    refactor: remove GenrePlaylistContext and introduce hooks for genre playlist fetching

---
+++ src/hooks/useGenrePlaylist.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useFetchWrapper } from "./useFetchWrapper";
+import { PaginatedResponse, PaginatedResponseSchema } from "@/types/api/pagination";
+import {
+  GenrePlaylistDetailedSchema,
+  GenrePlaylistDetailed,
+  GenrePlaylistSimpleSchema,
+  GenrePlaylistSimple,
+} from "@/models/domain/genre-playlist";
+
+export const useListGenrePlaylists = (page = 1, pageSize = 50) => {
+  const { fetch } = useFetchWrapper();
+  return useQuery<PaginatedResponse<GenrePlaylistSimple>>({
+    queryKey: ["genrePlaylists", page, pageSize],
+    queryFn: async () => {
+      const response = await fetch("genre-playlists", true, {}, { page, pageSize });
+      return PaginatedResponseSchema(GenrePlaylistSimpleSchema).parse(response);
+    },
+  });
+};
+
+export const useRetrieveGenrePlaylist = (id: string) => {
+  const { fetch } = useFetchWrapper();
+  return useQuery<GenrePlaylistDetailed>({
+    queryKey: ["genrePlaylists", id],
+    queryFn: async () => {
+      const response = await fetch(`genre-playlists/${id}`);
+      return GenrePlaylistDetailedSchema.parse(response);
+    },
+  });
+};
