53fdd7848a7529f5d8ad905e134f38750e8e782b
mignot <andreas.garcia@bodzify.com>
2025-04-25 01:29:18 +0200

    refactor: consolidate GenrePlaylist schemas and introduce createdOn and updatedOn fields

--- src/models/domain/genre-playlist.ts
+++ src/models/domain/genre-playlist.ts
@@ -2,9 +2,15 @@ import { z } from "zod";
 import { UuidResourceSchema } from "@/models/domain/uuid-resource";
 import { UploadedTrackPlaylistRelWithoutPlaylistSchema } from "@/models/domain/uploaded-track-playlist-rel";
 import { GenreMinimumSchema } from "@/models/domain/genre";
-export const GenrePlaylistDetailedSchema = UuidResourceSchema.extend({
-  uuid: z.string().uuid(),
+
+const baseSchema = UuidResourceSchema.extend({
   name: z.string(),
+});
+
+export const GenrePlaylistMinimumSchema = baseSchema;
+export type GenrePlaylistMinimum = z.infer<typeof GenrePlaylistMinimumSchema>;
+
+export const GenrePlaylistDetailedSchema = baseSchema.extend({
   uploadedTrackPlaylistRels: z.array(UploadedTrackPlaylistRelWithoutPlaylistSchema),
   uploadedTracksNotArchivedCountPublic: z.number().min(0),
   durationInSec: z.number().min(0),
@@ -13,23 +19,21 @@ export const GenrePlaylistDetailedSchema = UuidResourceSchema.extend({
   genre: GenreMinimumSchema,
   root: GenrePlaylistMinimumSchema,
   parent: GenrePlaylistMinimumSchema,
+  createdOn: z.string().datetime(),
+  updatedOn: z.string().datetime().nullable(),
 });
 
 export type GenrePlaylistDetailed = z.infer<typeof GenrePlaylistDetailedSchema>;
 
 export const GenrePlaylistSimpleSchema = GenrePlaylistDetailedSchema.omit({
-  children: true,
-  uploadedTracks: true,
-  uploadedTracksCount: true,
+  uploadedTrackPlaylistRels: true,
+  uploadedTracksNotArchivedCountPublic: true,
+  durationInSec: true,
+  durationStrInHourMinSec: true,
   uploadedTracksArchivedCount: true,
-  updatedOn: true,
+  genre: true,
+  root: true,
+  parent: true,
 });
 
 export type GenrePlaylistSimple = z.infer<typeof GenrePlaylistSimpleSchema>;
-
-export const GenrePlaylistMinimumSchema = GenrePlaylistDetailedSchema.pick({
-  uuid: true,
-  name: true,
-});
-
-export type GenrePlaylistMinimum = z.infer<typeof GenrePlaylistMinimumSchema>;
