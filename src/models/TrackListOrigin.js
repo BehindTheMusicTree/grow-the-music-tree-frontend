7fcbee40ff16196a1764cc0abd88bcb76eac0c44
mignot <andreas.garcia@bodzify.com>
2025-04-24 13:51:50 +0200

    refactor: migrate TrackListOrigin to TypeScript and update constructor

--- src/models/TrackListOrigin.js
+++
@@ -1,6 +0,0 @@
-export default class TrackListOrigin {
-  constructor(type, object) {
-    this.type = type;
-    this.object = object;
-  }
-}
---
+++ src/models/client/track-list/TrackListOrigin.ts
@@ -0,0 +1,5 @@
+import TrackList from "./TrackList";
+
+export default class TrackListOrigin {
+  constructor(public type: string, public object: TrackList) {}
+}
