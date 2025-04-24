42a29cf5d926755c5419f2eb8c323eff7f3de6ee
mignot <andreas.garcia@bodzify.com>
2025-04-25 01:00:13 +0200

    feat: add AlbumMinimum schema to UploadedTrackDetailed and introduce UploadedTrackSimpleWithoutAlbumWithPositionInAlbum schema

--- src/models/domain/uploaded-track.ts
+++ src/models/domain/uploaded-track.ts
@@ -1,12 +1,14 @@
 import { z } from "zod";
 import { ArtistMinimumSchema } from "./artist";
+import { AlbumMinimumSchema } from "./album";
+
 export const UploadedTrackDetailedSchema = z.object({
   uuid: z.string().uuid(),
   relativeUrl: z.string(),
   title: z.string(),
   file: z.string(),
   artists: z.array(ArtistMinimumSchema),
-  album: z.string(),
+  album: AlbumMinimumSchema,
   trackNumber: z.number().optional(),
   genre: z.string().optional(),
   rating: z.number().min(0).max(5).optional(),
@@ -19,3 +21,18 @@ export const UploadedTrackDetailedSchema = z.object({
 });
 
 export type UploadedTrackDetailed = z.infer<typeof UploadedTrackDetailedSchema>;
+
+export const UploadedTrackSimpleWithoutAlbumWithPositionInAlbumSchema = z.object({
+  uuid: z.string().uuid(),
+  title: z.string(),
+  trackNumber: z.number().optional(),
+  artists: z.array(ArtistMinimumSchema),
+  genre: z.string().optional(),
+  rating: z.number().min(0).max(5).optional(),
+  language: z.string().optional(),
+  playCount: z.number().default(0),
+});
+
+export type UploadedTrackSimpleWithoutAlbumWithPositionInAlbum = z.infer<
+  typeof UploadedTrackSimpleWithoutAlbumWithPositionInAlbumSchema
+>;
