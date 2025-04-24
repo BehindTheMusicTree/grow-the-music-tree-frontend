b4cae4a24fa5b303037157a0a5778d2d82e88e9c
mignot <andreas.garcia@bodzify.com>
2025-04-25 01:00:46 +0200

    feat: add AlbumDetailed and AlbumMinimum schemas with Zod validation

---
+++ src/models/domain/album.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+import { ArtistMinimumSchema } from "./artist";
+import { UploadedTrackSimpleWithoutAlbumWithPositionInAlbumSchema } from "./uploaded-track";
+export const AlbumDetailedSchema = z.object({
+  uuid: z.string().uuid(),
+  name: z.string(),
+  year: z.number().optional(),
+  album_artists: z.array(ArtistMinimumSchema),
+  library_tracks_sorted: z.array(UploadedTrackSimpleWithoutAlbumWithPositionInAlbumSchema),
+  library_tracks_count: z.number(),
+  library_tracks_archived_count: z.number(),
+  duration_in_sec: z.number(),
+  duration_str_in_hour_min_sec: z.string(),
+  createdOn: z.string().datetime(),
+  updatedOn: z.string().datetime(),
+});
+
+export type AlbumDetailed = z.infer<typeof AlbumDetailedSchema>;
+
+export const AlbumMinimumSchema = AlbumDetailedSchema.pick({
+  uuid: true,
+  name: true,
+  album_artists: true,
+});
+
+export type AlbumMinimum = z.infer<typeof AlbumMinimumSchema>;
