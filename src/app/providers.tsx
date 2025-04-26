2ff230307b725590d98e5f45308cbf7af7c22d06
mignot <andreas.garcia@bodzify.com>
2025-04-26 21:15:31 +0200

    feat: integrate React Query by adding QueryClientProvider and configuring query client

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,6 +7,8 @@ import { PopupProvider } from "@contexts/PopupContext";
 import { ConnectivityErrorProvider } from "@contexts/ConnectivityErrorContext";
 import { TrackListSidebarVisibilityProvider } from "@contexts/TrackListSidebarVisibilityContext";
 import { TrackListProvider } from "@contexts/TrackListContext";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@lib/query-client";
 
 interface ProvidersProps {
   children: NonNullable<ReactNode>;
@@ -14,16 +16,18 @@ interface ProvidersProps {
 
 export default function Providers({ children }: ProvidersProps) {
   return (
-    <SessionProvider>
-      <PlayerProvider>
-        <PopupProvider>
-          <ConnectivityErrorProvider>
-            <TrackListSidebarVisibilityProvider>
-              <TrackListProvider>{children}</TrackListProvider>
-            </TrackListSidebarVisibilityProvider>
-          </ConnectivityErrorProvider>
-        </PopupProvider>
-      </PlayerProvider>
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>
+        <PlayerProvider>
+          <PopupProvider>
+            <ConnectivityErrorProvider>
+              <TrackListSidebarVisibilityProvider>
+                <TrackListProvider>{children}</TrackListProvider>
+              </TrackListSidebarVisibilityProvider>
+            </ConnectivityErrorProvider>
+          </PopupProvider>
+        </PlayerProvider>
+      </SessionProvider>
+    </QueryClientProvider>
   );
 }
