03afce890dfda419d9eb7e9cb8f1c06e741a2477
mignot <andreas.garcia@bodzify.com>
2025-04-26 20:46:27 +0200

    feat: add TrackList subclasses for handling uploaded tracks and playlists

--- src/models/track-list/TrackList.ts
+++ src/models/track-list/TrackList.ts
@@ -1,6 +1,21 @@
 import { UploadedTrackDetailed } from "@schemas/uploaded-track/response";
-import TrackListOrigin from "./origin/TrackListOrigin";
+import TrackListOrigin, {
+  TrackListOriginFromUploadedTrack,
+  TrackListOriginFromPlaylist,
+} from "./origin/TrackListOrigin";
 
 export default class TrackList {
   constructor(public uploadedTracks: UploadedTrackDetailed[], public origin: TrackListOrigin) {}
+}
+
+export class TrackListFromUploadedTrack extends TrackList {
+  constructor(public uploadedTracks: UploadedTrackDetailed[], public origin: TrackListOriginFromUploadedTrack) {
+    super(uploadedTracks, origin);
+  }
+}
+
+export class TrackListFromPlaylist extends TrackList {
+  constructor(public uploadedTracks: UploadedTrackDetailed[], public origin: TrackListOriginFromPlaylist) {
+    super(uploadedTracks, origin);
+  }
 }
