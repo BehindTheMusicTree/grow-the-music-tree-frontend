69a252a5482c305bc50e1e56548865eee2c32158
mignot <andreas.garcia@bodzify.com>
2025-04-26 20:45:25 +0200

    feat: add uuid property to TrackListOrigin and its subclasses for better track identification

--- src/models/track-list/origin/TrackListOrigin.ts
+++ src/models/track-list/origin/TrackListOrigin.ts
@@ -3,7 +3,7 @@ import { PlaylistDetailed } from "@schemas/playlist";
 import { TrackListOriginType } from "./TrackListOriginType";
 
 export default class TrackListOrigin {
-  constructor(public type: TrackListOriginType, public label: string) {}
+  constructor(public type: TrackListOriginType, public label: string, public uuid: string) {}
 }
 
 export class TrackListOriginFromUploadedTrack extends TrackListOrigin {
@@ -12,13 +12,14 @@ export class TrackListOriginFromUploadedTrack extends TrackListOrigin {
       TrackListOriginType.UPLOADED_TRACK,
       `${uploadedTrack.title} by ${
         uploadedTrack.artists ? uploadedTrack.artists.map((artist) => artist.name).join(", ") : ""
-      }`
+      }`,
+      uploadedTrack.uuid
     );
   }
 }
 
 export class TrackListOriginFromPlaylist extends TrackListOrigin {
   constructor(public playlist: PlaylistDetailed) {
-    super(TrackListOriginType.PLAYLIST, playlist.name);
+    super(TrackListOriginType.PLAYLIST, playlist.name, playlist.uuid);
   }
 }
