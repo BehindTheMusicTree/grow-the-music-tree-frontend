210a6873675fbcc5903b7049a7f4b93a02f1df7e
mignot <andreas.garcia@bodzify.com>
2025-04-28 21:11:03 +0200

    refactor: update import paths for lineage and playlist schemas in CriteriaSimpleSchema

--- src/schemas/domain/criteria/response/simple.ts
+++ src/schemas/domain/criteria/response/simple.ts
@@ -1,10 +1,8 @@
 import { z } from "zod";
 import { UuidResourceSchema } from "@domain/uuid-resource";
-import {
-  CriteriaLineageRelWithoutDescendantsSchema,
-  CriteriaLineageRelWithoutAscendantsSchema,
-} from "../lineage-rel/detailed";
-import { GenrePlaylistMinimumSchema } from "@domain/criteria-playlist";
+import { CriteriaLineageRelWithoutDescendantsSchema } from "../lineage-rel/without-descendant";
+import { CriteriaLineageRelWithoutAscendantsSchema } from "../lineage-rel/without-ascendant";
+import { CriteriaPlaylistMinimumSchema } from "@domain/playlist/criteria-playlist/minimum";
 import { UploadedTrackMinimumSchema } from "@domain/uploaded-track/response/minimum";
 import { CrteriaMinimumSchema } from "./minimum";
 
@@ -15,7 +13,7 @@ export const CriteriaSimpleSchema = UuidResourceSchema.extend({
   descendants: z.array(CriteriaLineageRelWithoutAscendantsSchema),
   root: CrteriaMinimumSchema,
   children: z.array(CrteriaMinimumSchema),
-  criteriaPlaylist: GenrePlaylistMinimumSchema,
+  criteriaPlaylist: CriteriaPlaylistMinimumSchema,
   uploadedTracksNotArchived: z.array(UploadedTrackMinimumSchema),
   uploadedTracksCount: z.number(),
   uploadedTracksArchivedCount: z.number(),
