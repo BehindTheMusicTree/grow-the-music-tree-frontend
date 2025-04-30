2929a992e8992966f568820c235949c55685df9d
mignot <andreas.garcia@bodzify.com>
2025-04-30 16:33:08 +0200

    style: adjust layout and alignment in GenreTree component for improved UI

--- src/app/genre-tree/page.tsx
+++ src/app/genre-tree/page.tsx
@@ -59,8 +59,8 @@ export default function GenreTree() {
   }, [formErrors, showCreationPopup]);
 
   return (
-    <div className="mt-2 flex flex-col overflow-y-auto h-screen">
-      <div className="flex justify-end p-4 gap-2">
+    <div className="mt-4 flex flex-col h-screen">
+      <div className="flex justify-start">
         <button
           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 flex items-center gap-2 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:bg-blue-500"
           onClick={() => {
@@ -89,7 +89,7 @@ export default function GenreTree() {
           >
             +
           </div>
-          <div className="flex flex-col text-gray-800">
+          <div className="tree-container flex flex-col text-gray-800 min-w-max overflow-x-auto overflow-y-auto">
             {Object.entries(groupedGenrePlaylistsByRoot).map(([uuid, genrePlaylistTree]) => {
               // Wrap in array to match expected props type
               return (
