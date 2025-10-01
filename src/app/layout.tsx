0dfbe2e79690b5cd965f83079780ff96c484647c
mignot <andreas.garcia@bodzify.com>
2025-10-01 17:10:40 +0200

    feat: move metadata definition to layout.tsx and remove metadata.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
 import { Inter } from "next/font/google";
-import { metadata } from "./metadata";
+import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "@app/providers";
 import { PopupProvider } from "@contexts/PopupContext";
 import AppContent from "./AppContent";
+
+export const metadata: Metadata = {
+  title: "Music Tree",
+  description: "Your ultimate music guide",
+  icons: {
+    icon: "/icons8-tree-16.png",
+  },
+};
 
 const inter = Inter({
   subsets: ["latin"],
