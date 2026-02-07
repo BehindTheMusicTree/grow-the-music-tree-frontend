5e1f2944174d8ed4d440a80179a4c9e2a026f338
mignot <garcia.andreas.1991@gmail.com>
2026-02-07 19:32:13 +0100

    refactor: remove unnecessary React import in Player component

--- src/components/features/player/Player.tsx
+++ src/components/features/player/Player.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import { useState } from "react";
 import Image from "next/image";
 import { FaVolumeUp, FaVolumeMute, FaList } from "react-icons/fa";
 import { usePlayer, useCurrentTime } from "@contexts/PlayerContext";
