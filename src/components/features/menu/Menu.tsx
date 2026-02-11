c1befa0782199ac16e416337095821d5340900a1
mignot <garcia.andreas.1991@gmail.com>
2026-02-11 14:22:37 +0100

    refactor: consolidate menu groups into a single array for improved structure
    - Merge individual menu groups into a unified array for cleaner code organization.
    - Update Menu component to utilize the new menuGroup structure for rendering.

--- src/components/features/menu/Menu.tsx
+++ src/components/features/menu/Menu.tsx
@@ -5,44 +5,38 @@ import { FaSpotify, FaCloudUploadAlt, FaUser, FaList } from "react-icons/fa";
 import { MenuGroup } from "./MenuGroup";
 import { MENU_WIDTH } from "@lib/constants/layout";
 
-const menuGroups = {
-  genreTree: [
-    {
-      href: "/reference-genre-tree",
-      label: "Reference Genre Tree",
-      icon: <PiGraphLight className="text-xl" />,
-    },
-    {
-      href: "/me-genre-tree",
-      label: "My Genre Tree",
-      icon: <PiGraphLight className="text-xl" />,
-    },
-  ],
-  library: [
-    {
-      href: "/me-genre-playlists",
-      label: "My Genre Playlists",
-      icon: <FaList className="text-xl" />,
-    },
-    {
-      href: "/me-uploaded-library",
-      label: "My Uploaded Library",
-      icon: <FaCloudUploadAlt className="text-xl" />,
-    },
-    {
-      href: "/me-spotify-library",
-      label: "My Spotify Library",
-      icon: <FaSpotify className="text-xl" />,
-    },
-  ],
-  account: [
-    {
-      href: "/account",
-      label: "Account",
-      icon: <FaUser className="text-xl" />,
-    },
-  ],
-};
+const menuGroup = [
+  {
+    href: "/reference-genre-tree",
+    label: "Reference Genre Tree",
+    icon: <PiGraphLight className="text-xl" />,
+  },
+  {
+    href: "/me-genre-tree",
+    label: "My Genre Tree",
+    icon: <PiGraphLight className="text-xl" />,
+  },
+  {
+    href: "/me-genre-playlists",
+    label: "My Genre Playlists",
+    icon: <FaList className="text-xl" />,
+  },
+  {
+    href: "/me-uploaded-library",
+    label: "My Uploaded Library",
+    icon: <FaCloudUploadAlt className="text-xl" />,
+  },
+  {
+    href: "/me-spotify-library",
+    label: "My Spotify Library",
+    icon: <FaSpotify className="text-xl" />,
+  },
+  {
+    href: "/account",
+    label: "Account",
+    icon: <FaUser className="text-xl" />,
+  },
+];
 
 export default function Menu({ className }: { className?: string }) {
   return (
@@ -50,9 +44,7 @@ export default function Menu({ className }: { className?: string }) {
       className={`menu bg-black flex flex-col justify-start items-start p-2 h-full ${className}`}
       style={{ width: MENU_WIDTH }}
     >
-      <MenuGroup items={menuGroups.genreTree} />
-      <MenuGroup items={menuGroups.library} />
-      <MenuGroup items={menuGroups.account} />
+      <MenuGroup items={menuGroup} />
     </nav>
   );
 }
