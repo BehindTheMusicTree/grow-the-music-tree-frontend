fa4fd6bf22468c3c1c3463a4d0c386c26a75865e
mignot <garcia.andreas.1991@gmail.com>
2026-02-11 12:15:48 +0100

    fix: add missing slashes to endpoint URLs for Spotify and uploaded libraries

--- src/api/endpoints/library/spotify/endpoints.ts
+++ src/api/endpoints/library/spotify/endpoints.ts
@@ -1,6 +1,6 @@
 export const makeSpotifyEndpoints = (root: string) => ({
   list: () => `${root}/library/spotify/`,
-  detail: (uuid: string) => `${root}/library/spotify/${uuid}`,
+  detail: (uuid: string) => `${root}/library/spotify/${uuid}/`,
   syncQuick: () => `${root}/library/spotify/sync/quick/`,
   syncFull: () => `${root}/library/spotify/sync/full/`,
 });
