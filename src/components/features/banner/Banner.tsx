922b4e220a5c6a9763e45a91bec75ae024d08bc0
mignot <garcia.andreas.1991@gmail.com>
2026-02-11 10:27:30 +0100

    fix: update banner title for branding consistency
    - Change the banner title from "Music Tree" to "GrowTheMusicTree" to align with the updated branding.

--- src/components/features/banner/Banner.tsx
+++ src/components/features/banner/Banner.tsx
@@ -16,7 +16,7 @@ export default function Banner({ className }: BannerProps) {
           <div className="mr-3">
             <Image src={logo} alt="logo" width={48} style={{ height: "auto" }} />
           </div>
-          <h1 className="text-2xl font-bold">Music Tree</h1>
+          <h1 className="text-2xl font-bold">GrowTheMusicTree</h1>
         </div>
         <div className="search w-64 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
           <SearchForm />
