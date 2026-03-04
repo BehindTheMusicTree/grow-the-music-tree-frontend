78a885e35993cd8e4aa293b9709051bd31acc7e8
mignot <garcia.andreas.1991@gmail.com>
2026-03-04 10:18:49 +0100

    feat: add getArtistsDisplay function for artist formatting
    - Introduced a new `getArtistsDisplay` function in the artist display schema to format and join artist names from an array of `ArtistMinimum` objects, enhancing the presentation of artist data.
    - Removed the previous `getArtistsDisplay` function from the uploaded track display schema to streamline functionality.

--- src/schemas/domain/uploaded-track/display.ts
+++
@@ -1,5 +0,0 @@
-import { UploadedTrackMinimum } from "./response/minimum";
-
-export function getArtistsDisplay(uploadedTrack: UploadedTrackMinimum) {
-  return uploadedTrack.artists?.map((artist) => artist.name).join(", ") ?? "";
-}
