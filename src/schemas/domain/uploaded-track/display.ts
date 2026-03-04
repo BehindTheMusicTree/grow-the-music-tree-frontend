c11b08233b521e3f5dd95f30c55c340c355987d3
mignot <garcia.andreas.1991@gmail.com>
2026-03-04 10:12:53 +0100

    fix: ensure getArtistsDisplay returns an empty string for undefined artists
    - Updated the `getArtistsDisplay` function to return an empty string instead of `undefined` when there are no artists, improving the function's robustness and preventing potential display issues.

--- src/schemas/domain/uploaded-track/display.ts
+++ src/schemas/domain/uploaded-track/display.ts
@@ -1,5 +1,5 @@
 import { UploadedTrackMinimum } from "./response/minimum";
 
 export function getArtistsDisplay(uploadedTrack: UploadedTrackMinimum) {
-  return uploadedTrack.artists?.map((artist) => artist.name).join(", ");
+  return uploadedTrack.artists?.map((artist) => artist.name).join(", ") ?? "";
 }
